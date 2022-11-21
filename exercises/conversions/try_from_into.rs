01aff0b3a0cebe79682be2f7c70ae1f5a8d40c6b
vdong <sunvdong@qq.com>
2022-11-22 00:28:01 +0800

    修改了try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -36,18 +36,32 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
-        let (red, green, blue) = tuple;
-        for c in [red, green, blue] {
-            if c < 0 || c > 255 {
-                return Err(IntoColorError::IntConversion);
-            }
-        }
+        // let (red, green, blue) = tuple;
+        // for c in [red, green, blue] {
+        //     if c < 0 || c > 255 {
+        //         return Err(IntoColorError::IntConversion);
+        //     }
+        // }
 
-        Ok(Color {
-            red: tuple.0 as u8,
-            green: tuple.1 as u8,
-            blue: tuple.2 as u8,
-        })
+        // Ok(Color {
+        //     red: tuple.0 as u8,
+        //     green: tuple.1 as u8,
+        //     blue: tuple.2 as u8,
+        // })
+
+        let red: u8 = tuple
+            .0
+            .try_into()
+            .map_err(|_| IntoColorError::IntConversion)?;
+        let green: u8 = tuple
+            .1
+            .try_into()
+            .map_err(|_| IntoColorError::IntConversion)?;
+        let blue: u8 = tuple
+            .2
+            .try_into()
+            .map_err(|_| IntoColorError::IntConversion)?;
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -55,17 +69,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
-        for c in arr {
-            if c < 0 || c > 255 {
-                return Err(IntoColorError::IntConversion);
-            }
-        }
-
-        Ok(Color {
-            red: arr[0] as u8,
-            green: arr[1] as u8,
-            blue: arr[2] as u8,
-        })
+        Color::try_from((arr[0], arr[1], arr[2]))
     }
 }
 
@@ -76,17 +80,7 @@ impl TryFrom<&[i16]> for Color {
         if slice.len() != 3 {
             return Err(IntoColorError::BadLen);
         }
-        for c in slice {
-            if *c < 0 || *c > 255 {
-                return Err(IntoColorError::IntConversion);
-            }
-        }
-
-        Ok(Color {
-            red: slice[0] as u8,
-            green: slice[1] as u8,
-            blue: slice[2] as u8,
-        })
+        Color::try_from((slice[0], slice[1], slice[2]))
     }
 }
 
