7ff4f375452ae4f01c10f461e559f0a153198379
vdong <sunvdong@qq.com>
2022-11-22 00:09:34 +0800

    clippy3 修改

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,9 +4,8 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        //my_option.unwrap();
-        panic!("none");
+    if let Some(()) = my_option {
+        my_option.unwrap();
     }
 
     let my_arr = &[-1, -2, -3, -4, -5, -6];
